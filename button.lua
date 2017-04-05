191d7f9a9a23f48559191d5523b712e8699e0d08
LetsOffBrains <darkhorseaka@ya.ru>
2017-04-05 22:00:03 +0700

    included some style =P

--- button.lua
+++ button.lua
@@ -7,7 +7,7 @@ function button:new(x, y, p)
   t.char= p or "!"
   t.size = 40
   t.color = {255, 255, 255, 255}
-  
+
   return setmetatable(t,{__index = self})
 end
 
@@ -15,26 +15,24 @@ function button:draw()
 love.graphics.setColor(self.color)
 love.graphics.rectangle("line", self.x, self.y, self.size, self.size)
 love.graphics.printf( self.char, self.x, self.y+self.size/2-mainFont:getHeight( )/2, self.size, "center" )
-	if self.color[2] ~= 255 then
-		love.graphics.line(self.x, self.y, self.x + self.size, self.y + self.size)
-		love.graphics.line(self.x + self.size, self.y, self.x, self.y + self.size)
-	end
+
+if self.color[2] ~= 255 then
+	love.graphics.line(self.x, self.y, self.x + self.size, self.y + self.size)
+	love.graphics.line(self.x + self.size, self.y, self.x, self.y + self.size)
+end
+
 end
 
 function button:mousereleased(x, y, button)
-	if  x >= self.x and x <= self.x + self.size
-	and y >= self.y and y <= self.y + self.size   then
-		if button == 1 then
-			self.color = {255, 0, 0, 255}
-			
-			text=text..self.char
-		else
-			self.color = {255, 255, 255, 255}
-			
-		end
-		
+if x >= self.x and x <= self.x + self.size
+and y >= self.y and y <= self.y + self.size   then
+	if button == 1 then
+		self.color = {255, 0, 0, 255}
+		text=text..self.char
+	else
+		self.color = {255, 255, 255, 255}
 	end
-	
-		
+end
+
 end
 
--- main.lua
+++ main.lua
@@ -1,80 +1,82 @@
 function extended (child, parent)
-    setmetatable(child,{__index = parent})
+setmetatable(child,{__index = parent})
 end
 
 require("human")
 require("button")
 
 function love.load(arg)
-	text= " "
-	mainFont = love.graphics.newFont("arial.ttf", 20);
-  --if arg[#arg] == "-debug" then require("mobdebug").start() end
-  h = human:new(400, 160, 0)
-	--b = button:new(600, 500,"Q")
-	buttons= {}
-	--table.insert(buttons,b)
-	kb = {
-		{ 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ' },
-		{ 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э' },
-		{ 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю' },
-		size = 40,
-		padding = 5,
-		spacing = {
-			120,
-			153,
-			198
-		},
-		top = 400
-	}
-	--require("mobdebug").on()
-	for i = 1, 3 do
-		for j = 1, #kb[i] do
-			b=button:new((kb.size + kb.padding) * (j - 1) + kb.spacing[i], (kb.size + kb.padding) * (i - 1) + kb.top, kb[i][j])
+--if arg[#arg] == "-debug" then require("mobdebug").start() end
+text= " "
+mainFont = love.graphics.newFont("arial.ttf", 20);
+h = human:new(400, 160, 0)
+buttons= {}
+kb = {
+	{ 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ' },
+	{ 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э' },
+	{ 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю' },
+	size = 40,
+	padding = 5,
+	spacing = {
+		120,
+		153,
+		198
+	},
+	top = 400
+}
 
-			table.insert(buttons,b)
-			--print(b.x.." "..b.y)
-		end
+for i = 1, 3 do
+	for j = 1, #kb[i] do
+		b=button:new((kb.size + kb.padding) * (j - 1) + kb.spacing[i], (kb.size + kb.padding) * (i - 1) + kb.top, kb[i][j])
+		table.insert(buttons,b)
+		--print(b.x.." "..b.y)
 	end
-	--require("mobdebug").off()
-	print(buttons[1].x.." "..buttons[1].y.." "..buttons[1].char)
-	print(buttons[32].x.." "..buttons[32].y.." "..buttons[32].char)
+end
+
+print(buttons[1].x.." "..buttons[1].y.." "..buttons[1].char)
+print(buttons[32].x.." "..buttons[32].y.." "..buttons[32].char)
 end
 
 function love.update(dt)
-  mx, my = love.mouse.getPosition( )
-
+--require("mobdebug").on()
+mx, my = love.mouse.getPosition( )
+--require("mobdebug").off()
 end
 
 function love.draw()
-
-	love.graphics.setFont(mainFont);
-	love.graphics.setColor(255, 255, 255, 255)
-	h:draw()
-	for i=1, #buttons do
+love.graphics.setFont(mainFont);
+love.graphics.setColor(255, 255, 255, 255)
+h:draw()
+for i=1, #buttons do
 	buttons[i]:draw()
-	end
-	love.graphics.print(text, 100, 500)
+end
+love.graphics.print(text, 100, 500)
 end
 
 
 function love.mousepressed(x, y, button, istouch)
-  if button == 1 then -- the primary button
+if button == 1 then -- the primary button
 
-  end
-  if button == 2 then
+end
 
-  end
+if button == 2 then
+
+end
 end
 
 function love.mousereleased(x, y, button)
-  h.parts.number = (h.parts.number + 1) % 7
-	for i=1, #buttons do
+h.parts.number = (h.parts.number + 1) % 7
+
+for i=1, #buttons do
 		buttons[i]:mousereleased(x,y,button)
-	end
-  if button == 1 then
+end
 
-  end
-  if button == 2 then
+if button == 1 then
 
-  end
+end
+
+if button == 2 then
+
+end
+
 end
