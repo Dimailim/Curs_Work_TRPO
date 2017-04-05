407ec5c1e3c40431cbfc3439407319c5293d7842
LetsOffBrains <darkhorseaka@ya.ru>
2017-04-05 21:46:37 +0700

    Changed the way to init buttons

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 function extended (child, parent)
-    setmetatable(child,{__index = parent}) 
+    setmetatable(child,{__index = parent})
 end
 
 require("human")
@@ -16,22 +16,21 @@ function love.load(arg)
 	kb = {
 		{ 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ' },
 		{ 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э' },
-		{ 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю' }
+		{ 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю' },
+		size = 40,
+		padding = 5,
+		spacing = {
+			120,
+			153,
+			198
+		},
+		top = 400
 	}
 	--require("mobdebug").on()
-	for i = 1, 3 do 
-		for j = 1, #kb[i] do 
-			
-			if i==1 then 
-				b=button:new(45*j+120,(45*i)+400, kb[i][j])
-			end
-			if i==2 then 
-				b=button:new(45*j+153,(45*i)+400, kb[i][j])
-			end
-			if i==3 then 
-				b=button:new(45*j+198,(45*i)+400, kb[i][j])
-			end
-			
+	for i = 1, 3 do
+		for j = 1, #kb[i] do
+			b=button:new((kb.size + kb.padding) * (j - 1) + kb.spacing[i], (kb.size + kb.padding) * (i - 1) + kb.top, kb[i][j])
+
 			table.insert(buttons,b)
 			--print(b.x.." "..b.y)
 		end
@@ -47,11 +46,11 @@ function love.update(dt)
 end
 
 function love.draw()
-	
+
 	love.graphics.setFont(mainFont);
 	love.graphics.setColor(255, 255, 255, 255)
 	h:draw()
-	for i=1, #buttons do 
+	for i=1, #buttons do
 	buttons[i]:draw()
 	end
 	love.graphics.print(text, 100, 500)
@@ -62,20 +61,20 @@ function love.mousepressed(x, y, button, istouch)
   if button == 1 then -- the primary button
 
   end
-  if button == 2 then 
-    
+  if button == 2 then
+
   end
 end
 
 function love.mousereleased(x, y, button)
   h.parts.number = (h.parts.number + 1) % 7
-	for i=1, #buttons do 
+	for i=1, #buttons do
 		buttons[i]:mousereleased(x,y,button)
-	end   
+	end
   if button == 1 then
-   
+
   end
-  if button == 2 then 
-		
+  if button == 2 then
+
   end
 end
