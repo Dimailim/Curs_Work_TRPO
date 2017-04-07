27047fd07b42c4951e7ceb3b955db23a7cd1a8f5
LetsOffBrains <darkhorseaka@ya.ru>
2017-04-07 14:39:46 +0700

    "included states"

--- button.lua
+++ button.lua
@@ -12,27 +12,27 @@ function button:new(x, y, p)
 end
 
 function button:draw()
-love.graphics.setColor(self.color)
-love.graphics.rectangle("line", self.x, self.y, self.size, self.size)
-love.graphics.printf( self.char, self.x, self.y+self.size/2-mainFont:getHeight( )/2, self.size, "center" )
+	love.graphics.setColor(self.color)
+	love.graphics.rectangle("line", self.x, self.y, self.size, self.size)
+	love.graphics.printf( self.char, self.x, self.y+self.size/2-mainFont:getHeight( )/2, self.size, "center" )
 
-if self.color[2] ~= 255 then
-	love.graphics.line(self.x, self.y, self.x + self.size, self.y + self.size)
-	love.graphics.line(self.x + self.size, self.y, self.x, self.y + self.size)
-end
+	if self.color[2] ~= 255 then
+		love.graphics.line(self.x, self.y, self.x + self.size, self.y + self.size)
+		love.graphics.line(self.x + self.size, self.y, self.x, self.y + self.size)
+	end
 
 end
 
 function button:mousereleased(x, y, button)
-if x >= self.x and x <= self.x + self.size
-and y >= self.y and y <= self.y + self.size   then
-	if button == 1 then
-		self.color = {255, 0, 0, 255}
-		text=text..self.char
-	else
-		self.color = {255, 255, 255, 255}
+	if x >= self.x and x <= self.x + self.size
+	and y >= self.y and y <= self.y + self.size   then
+		if button == 1 then
+			self.color = {255, 0, 0, 255}
+			text=text..self.char
+		else
+			self.color = {255, 255, 255, 255}
+		end
 	end
-end
 
 end
 
