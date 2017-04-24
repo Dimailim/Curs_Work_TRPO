e6a9ca5bcd42c1d5671a739d877dc9ea648670be
LetsOffBrains <darkhorseaka@ya.ru>
2017-04-24 22:52:37 +0700

    Add exe

--- game.lua
+++ game.lua
@@ -30,9 +30,9 @@ function Game.load()
 end
 --
 function Game.update(dt)
-	for i = 1, #buttons do 
-		buttons[i]:update(x, y, button) 
-	end 
+	for i = 1, #buttons do
+		buttons[i]:update(x, y, button)
+	end
 end
 --
 function Game.draw()
